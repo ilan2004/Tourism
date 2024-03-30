bc805407d83fcb478af8495888ccfd398a01441f
Ilan Usman <ilanusman03@gmail.com>
2024-03-30 17:16:01 +0530

    error fixing

--- src/Components/Bookcomp/Options/BookCard/html.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import htmlContent from '../room1/index.html';
-const html = () => {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  )
-}
-export default html;
