df19c74694f8c4ff446544caae563e6b9052b2af
Ilan Usman <ilanusman03@gmail.com>
2024-03-30 18:26:05 +0530

    whyy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite';
-import { html } from 'vite-plugin-html';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // other configurations
-  plugins: [
-    html({
-      minify: true, // Optional minification
-      inject: {
-        injectData: {
-          title: 'My App' // Optionally inject data into HTML
-        }
-      },
-      entryFile: 'A:/Git/Tourism/src/Components/Bookcomp/Options/room1/hotel1.html' // Specify entry HTML file
-    })
-  ]
-});
+  plugins: [react()],
+  assetsInclude: ['**/*.html']
+})
