6ee85915570c3172232104a531f7f0c4e10ecb9b
Ilan Usman <ilanusman03@gmail.com>
2024-04-01 20:42:36 +0530

    controls

--- src/Components/Homecomp/Hero/Hero.jsx
+++ src/Components/Homecomp/Hero/Hero.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
 import { NavLink } from 'react-router-dom';
-import main from '../../../assets/main1.mp4'
+import main from '../../../assets/main1.mp4';
+
 const Hero = () => {
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
   return (
     <div className='Hero'>
-        <video autoPlay loop muted className='back'>
-            <source src={main} type="video/mp4" />
-        </video>
-    <div class="content"><h1>WHETRIP</h1>
-    <NavLink to="/Booking"> <a class ="explore">Explore</a></NavLink>
-     </div>
-
-
+      <video autoPlay loop muted className='back' controls={!isMobile}>
+        <source src={main} type="video/mp4" />
+      </video>
+      <div class="content">
+        <h1>WHETRIP</h1>
+        <NavLink to="/Booking">
+          <a class="explore">Explore</a>
+        </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default Hero;
